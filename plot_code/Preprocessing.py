5dca0f411a0701918a5470bbe29fff29f8c5cbdb
r05229014 <r05229014@ntu.edu.tw>
2018-10-18 16:46:35 +0800

    update

--- plot_code/Preprocessing.py
+++ plot_code/Preprocessing.py
@@ -13,14 +13,12 @@ def load_alldata(path_x, path_y):
     l = sorted(l)
 
     # processing X
-    X_train = np.zeros((1,34,33,33,5))
-    X_test = np.zeros((1,34,33,33,5))
-    y_train = np.zeros((1,34,33,33))
-    y_test = np.zeros((1,34,33,33))
+    X = np.zeros((1,34,33,33,5))
+    y = np.zeros((1,34,33,33))
 
     for name in l:
 
-        if name in ['n01.pkl', 's01.pkl']:
+        if name in ['n01.pkl']:
             print('Now loading : %s' %name)
             
             # X
@@ -34,19 +32,6 @@ def load_alldata(path_x, path_y):
             with open(path_y + name, 'rb') as f:
                 case_y = pickle.load(f)
             wqv = case_y['wqv']
-            
-            # shuffle
-            indices = np.arange(temp.shape[0])  
-            nb_test_samples = int(TEST_SPLIT * temp.shape[0])
-            np.random.shuffle(indices)
-            temp = temp[indices]
-            train = temp[nb_test_samples:]
-            test = temp[0:nb_test_samples]
-
-            wqv = wqv[indices]
-            train_wqv = wqv[nb_test_samples:]
-            test_wqv = wqv[0:nb_test_samples]
-
 
         else:
             print('Now loading : %s' %name)
@@ -61,34 +46,34 @@ def load_alldata(path_x, path_y):
                 case_y = pickle.load(f)
             wqv = case_y['wqv'][37:]
 
-            # shuffle
-            indices = np.arange(temp.shape[0])  
-            nb_test_samples = int(TEST_SPLIT * temp.shape[0])
-            np.random.shuffle(indices)
-            temp = temp[indices]
-            train = temp[nb_test_samples:]
-            test = temp[0:nb_test_samples]
+        X = np.concatenate((X, temp), axis=0)
+        y = np.concatenate((y, wqv), axis=0)
+    X = X[1:]
+    y = y[1:]
 
-            wqv = wqv[indices]
-            train_wqv = wqv[nb_test_samples:]
-            test_wqv = wqv[0:nb_test_samples]
+    # shuffle
+    indices = np.arange(X.shape[0])
+    nb_test_samples = int(TEST_SPLIT * X.shape[0])
+    random.seed(777)
+    random.shuffle(indices)
+    X = X[indices]
+    X_train = X[nb_test_samples:]
+    X_test = X[0:nb_test_samples]
+    y = y[indices]
+    y_train = y[nb_test_samples:]
+    y_test = y[0:nb_test_samples]
 
-        X_train = np.concatenate((X_train, train), axis=0)
-        X_test = np.concatenate((X_test, test), axis=0)
-        y_train = np.concatenate((y_train, train_wqv), axis=0)
-        y_test = np.concatenate((y_test, test_wqv), axis=0)
-
-    X_train = X_train[1:]
-    X_test = X_test[1:]
-    y_train = y_train[1:]
-    y_test = y_test[1:]
     print('X_train shape is : ', X_train.shape)
-    print('X_test shape is : ', X_test.shape)
     print('y_train shape is : ', y_train.shape)
+    print('X_test shape is : ', X_test.shape)
     print('y_test shape is : ', y_test.shape)
 
     return X_train, X_test, y_train, y_test
 
+
+#dirx = '../feature/'
+#diry = '../target/'
+#load_alldata(dirx, diry)
 
 def Preprocessing_Linear(X_train, X_test, y_train, y_test):
     X_train = X_train.reshape(-1,5)
--- plot_code/paper_plot.py
+++ plot_code/paper_plot.py
@@ -5,45 +5,74 @@ import matplotlib.cm as cm
 import matplotlib.mlab as mlab
 import os
 import sys
+import random
 
-def paper_plot(load_path, case, save_path, test):
-    print('Now plotting %s' %case)
-    with open(load_path + case + '.pkl', 'rb') as f:
-        if test == False:
-            wqv = pickle.load(f)['wqv']
-        else:
-            wqv = pickle.load(f)
+from Preprocessing import *
+
+def load_tags():
+    TEST_SPLIT = 0.2
+    ls = sorted(os.listdir('./w_greater/'))
+    
+    tags = np.zeros((1,33,33))
+    for name in ls:
+        if name == 'n01.npy':
+            print('Now loading : %s' %name)
+            
+            tag = np.load('./w_greater/' + name)
         
+        else:
+            print('Now loading : %s' %name)
+            tag = np.load('./w_greater/' + name)[37:]
+        tags = np.concatenate((tags, tag), axis=0)
+    tags = tags[1:]
+
+    # shuffle
+    indices = np.arange(tags.shape[0])
+    nb_test_samples = int(TEST_SPLIT * tags.shape[0])
+    random.seed(777)
+    random.shuffle(indices)
+    tags = tags[indices]
+    tags_train = tags[nb_test_samples:]
+    tags_test = tags[0:nb_test_samples]
+
+    print('tags_train shape is : ', tags_train.shape)
+    print('tags_test shape is : ', tags_test.shape)
     
-    wqv_3000 = wqv[:,12,:,:]
-    d = np.load('./w_greater/' + case + '.npy')
-    d2 = np.invert(d)
-    dd = d.reshape(73,1,33,33)
-    dd2 = np.invert(dd)
-    
-    vir_cloudy = dd * wqv
-    hor_cloudy = d * wqv_3000
-    vir_nocloud = dd2 * wqv
-    hor_nocloud = dd * wqv_3000
+    return tags_train, tags_test
+
+
+def paper_plot(save_path, tags_test, y_test):
+
+    y_test_3000 = y_test[:,12,:,:]
+
+    # tags (boolean array)
+    tags_test = tags_test.astype(bool)
+    cloudy_vir_tags = tags_test.reshape(137,1,33,33)
+    nocloud_vir_tags = np.invert(cloudy_vir_tags)
+
+    vir_cloudy = cloudy_vir_tags * y_test
+    vir_nocloud = nocloud_vir_tags * y_test
+
+    # plot
     z = np.loadtxt('../z.txt')
-    
     x = np.arange(33)
     xx,yy = np.meshgrid(x,x)
-    
-    for t in range(73):
+
+    for t in range(vir_cloudy.shape[0]):
+        print('ploting testing sampling %s' %t)
         plt.figure(t, figsize=(8,10))
-        plt.title('t = %s' %t)
+        plt.title('testing sample %s' %t)
         ax1 = plt.subplot2grid((4,2), (0,0), colspan=2, rowspan=2)
         ax1.axis('off')
-        cs = ax1.contourf(xx,yy,wqv_3000[t]*2.5*10**6, cmap=cm.Blues, vmax=8000, vmin=0, levels = np.arange(0, 8001, 1000))
+        cs = ax1.contourf(xx,yy,y_test_3000[t]*2.5*10**6, cmap=cm.Blues, vmax=8000, vmin=0, levels = np.arange(0, 8001, 1000))
         m = plt.cm.ScalarMappable(cmap=cm.Blues)
-        m.set_array(wqv_3000)
+        m.set_array(y_test_3000)
         m.set_clim(0, 10001)
         cbar = plt.colorbar(cs)
         cbar.set_ticks([0,2000,4000,6000,8000])
         cbar.set_ticklabels([0,2000,4000,6000,8000])
         
-        number = np.sum(dd[t])
+        number = np.sum(cloudy_vir_tags[t])
         #print(number)
         if number != 0:
             vir = np.sum(np.sum(vir_cloudy[t],axis=-1), axis=-1) / number
@@ -56,7 +85,7 @@ def paper_plot(load_path, case, save_path, test):
         ax2.set_xlim(-0.0001*2.5*10**6, 0.003*2.5*10**6)
         ax2.set_ylim(z[0]/1000, z[-1]/1000)
         
-        number2 = np.sum(dd2[t])
+        number2 = np.sum(nocloud_vir_tags[t])
         #print(number2)
         if number2 != 0:
             vir2 = np.sum(np.sum(vir_nocloud[t],axis=-1), axis=-1) / number2
@@ -67,15 +96,15 @@ def paper_plot(load_path, case, save_path, test):
         ax3.plot(vir2*2.5*10**6, z/1000)
         ax3.set_xlim(-0.0001*2.5*10**6, 0.003*2.5*10**6)
         ax3.set_ylim(z[0]/1000, z[-1]/1000)
-    
-        plt.savefig(save_path + case +'/'+'{:0>5d}'.format(t) + '.png')
+
+        plt.savefig(save_path + 'testing' +'/'+'{:0>5d}'.format(t) + '.png')
         plt.close()
 
-if __name__ == '__main__':
+
+def main():
     plot_tar = sys.argv[1]
 
     if plot_tar == 'wqv':
-        load_path = '../target/'
         save_path = '../img/paper/'+ plot_tar +'/'
         test = False
     elif plot_tar == 'Linear':
@@ -89,12 +118,16 @@ if __name__ == '__main__':
     else:
         sys.exit('Please input wqv, Linear, DNN or RNN you want to plot!!!')
 
-    all_case = os.listdir(load_path)
-    for case in all_case:
-        caseName = case[0:3]
-        print(caseName)
-        dir_name = save_path + caseName
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
-        paper_plot(load_path, caseName, save_path, test)
-    
+
+    dir_name = save_path + 'testing/'
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    paper_plot(save_path, test)
+
+
+dirx = '../feature/'
+diry = '../target/'
+X_train, X_test, y_train, y_test = load_alldata(dirx, diry)
+tags_train, tags_test = load_tags()
+save_path = '../img/paper/'+ 'wqv' +'/'
+paper_plot(save_path, tags_test, y_test)
